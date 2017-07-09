5bad43f17bf377de2b001d2e1efe93c2fbec0a41
Aleksander Mangierowski <mangerovskii@gmail.com>
2017-07-09 21:52:34 +0300

    The beginning of nineSquares branch

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var slideWidth=772;
-var centerHeight = 600;
-var sliderTimer;
-$(function(){
-    $('.ul-center').width($('.ul-center').children().length * centerHeight);
-        sliderTimer=setInterval(nextSlide,1000);
-        // $('.viewport').hover(function(){
-        //     clearInterval(sliderTimer);
-        // },function(){
-        //     sliderTimer=setInterval(nextSlide,1000);
-        // });
-    });
+// var slideWidth=772;
+// var centerHeight = 600;
+// var sliderTimer;
+// $(function(){
+//     $('.ul-center').width($('.ul-center').children().length * centerHeight);
+//         sliderTimer=setInterval(nextSlide,1000);
+//         // $('.viewport').hover(function(){
+//         //     clearInterval(sliderTimer);
+//         // },function(){
+//         //     sliderTimer=setInterval(nextSlide,1000);
+//         // });
+//     });
 
-function nextSlide(){
-    var currentSlide=parseInt($('.ul-center').data('current'));
-    currentSlide++;
+// function nextSlide(){
+//     var currentSlide=parseInt($('.ul-center').data('current'));
+//     currentSlide++;
     
-    if (currentSlide == $('.ul-center').children().length)
-    {
-        currentSlide=0; 
-        // alert(0);  
-    }
+//     if (currentSlide == $('.ul-center').children().length)
+//     {
+//         currentSlide=0; 
+//         // alert(0);  
+//     }
     
-    $('.ul-center').animate({top: -currentSlide*centerHeight - 55},300).data('current',currentSlide);
-}
+//     $('.ul-center').animate({top: -currentSlide*centerHeight - 55},300).data('current',currentSlide);
+// }
